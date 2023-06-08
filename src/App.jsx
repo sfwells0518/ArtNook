8ff4d54603c5996e1307313400e3d373fdd1e0a6
Sam Wells <sfwells0518@gmail.com>
2023-06-08 11:37:13 -0500

    create shell jsx files Content, Header, Footer, PaintingsIndex, PaintingsShow and rename ChatGPT file from app.jsx to Chat.jsx. New App.jsx file added with correct content

--- src/App.jsx
+++ src/App.jsx
@@ -1,51 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
-import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
+import { BrowserRouter } from "react-router-dom";
+import { Header } from "./Header";
+import { Content } from "./Content";
+import { Footer } from "./Footer";
+import { Chat } from "./Chat";
 
 function App() {
-  const [typing, setTyping] = useState(false);
-  const [messages, setMessages] = useState([
-    {
-      message: "Hello, I am ChatGPT!",
-      sender: "ChatGPT" 
-
-    }
-  ])
-
-  const handleSend = async (message) => {
-    const newMessage = {
-      message: message,
-      sender: "user",
-      direction: "outgoing"
-    }
-
-    const newMessages = [...messages, newMessage];
-    setMessages(newMessages);
-
-    setTyping(true);
-  }
-
   return (
-    <div className="App">
-      <div style={{ position: "relative", height: "800px", width: "700px" }}>
-        <MainContainer>
-          <ChatContainer>
-            <MessageList
-            typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing"/> : null}
-            >
-              {messages.map((message, i) => {
-                return <Message key={i} model={message} />
-              })}
-            </MessageList>
-            <MessageInput placeholder='Type message here' onSend={handleSend} />
-          </ChatContainer>
-        </MainContainer>
-      </div>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Chat />
+        <Content />
+        <Footer />
+      </BrowserRouter> 
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/Chat.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
+import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from "@chatscope/chat-ui-kit-react"
+
+export function Chat() {
+  const [typing, setTyping] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hello, I am ChatGPT!",
+      sender: "ChatGPT" 
+
+    }
+  ])
+
+  const handleSend = async (message) => {
+    const newMessage = {
+      message: message,
+      sender: "user",
+      direction: "outgoing"
+    }
+
+    const newMessages = [...messages, newMessage];
+    setMessages(newMessages);
+
+    setTyping(true);
+  }
+
+  return (
+    <div className="App">
+      <div style={{ position: "relative", height: "800px", width: "700px" }}>
+        <MainContainer>
+          <ChatContainer>
+            <MessageList
+            typingIndicator={typing ? <TypingIndicator content="ChatGPT is typing"/> : null}
+            >
+              {messages.map((message, i) => {
+                return <Message key={i} model={message} />
+              })}
+            </MessageList>
+            <MessageInput placeholder='Type message here' onSend={handleSend} />
+          </ChatContainer>
+        </MainContainer>
+      </div>
+    </div>
+  )
+}
+
+export default App
